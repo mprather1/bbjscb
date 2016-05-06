f6dfabe1e30ead024aaff428ad7fb0208fc63c30
Michael Prather <mprather1@gmail.com>
2016-05-05 19:38:09 -0500

    item view

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ var InvoiceItemModel = Backbone.Model.extend({
 var InvoiceItemCollection = Backbone.Collection.extend({
   model: InvoiceItemModel
 });
+
+var InvoiceItemView = Backbone.View.extend({
+
+  tagName: 'tr',
+
+  render: function() {
+    $(this.el).html(_.map([
+      this.model.get('quantity'),
+      this.model.get('description'),
+      this.model.get('price'),
+      this.modle.calculateAmount(),
+    ], function(val, key) {
+      return '<td>' + val + '</td>'
+    }));
+    return this;
+  }
+});
